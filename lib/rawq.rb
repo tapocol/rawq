8cec29459af8648461b3583c509992ac13fb90e1
Craig Jackson <tapocol@gmail.com>
2011-10-20 21:45:34 -0600

    Added RawQ::Version and removed default test.

---
+++ lib/rawq.rb
@@ -0,0 +1,2 @@
+require "rawq/version"
+
---
+++ lib/rawq/version.rb
@@ -0,0 +1,10 @@
+module RawQ
+  module Version
+    MAJOR = 0
+    MINOR = 1
+    PATCH = 1
+    BUILD = nil
+
+    STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
+  end
+end
