1e5e5f67b6f65991bcbfc23b6683650e32337413
Craig Jackson <tapocol@gmail.com>
2012-01-31 19:50:49 -0700

    Add media_type to Media.

--- lib/rawq/collections/media.rb
+++ lib/rawq/collections/media.rb
@@ -3,6 +3,9 @@ module RawQ
     include Mongoid::Document
     store_in :media
     embeds_many :sources, :class_name => "RawQ::Source"
+    field :media_type, :type => String
+
+    validates_inclusion_of :media_type, :in => ["audio", "video"]
   end
 end
 
